0a23fa5b62ce046d68f11494544849744d2c52ec
kggu <miikkakortelainen1@gmail.com>
2020-09-06 14:43:13 +0300

    Add initial layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import CSVReader from "react-csv-reader";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [index, setIndex] = useState();
+  const [input, setInput] = useState();
+  const [converted, setConverted] = useState();
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleFileChange = (e) => {
+    console.log(e.target.files[0]);
+  };
+
+  useEffect(() => {
+    console.log("INPUT: ");
+    console.log(input);
+  }, [input]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p>
+        <code>unix epoch {"->"} timestamp</code>
+      </p>
+      <div className="controls">
+        <div className="input-control">
+          <div className="input-header">
+            <p>
+              <code>original |</code>
+            </p>
+            <p>
+              <code>index:</code>
+            </p>
+            <input type="text" style={{ width: "2em" }}></input>
+            <button className="input-header-button">
+              <code>select from file</code>
+            </button>
+          </div>
+
+          <textarea
+            onChange={handleInputChange}
+            type="text"
+            className="input-field"
+            readOnly
+          >
+            {input}
+          </textarea>
+        </div>
+        <div className="input-control">
+          <div className="input-header">
+            <p>
+              <code>converted |</code>
+            </p>
+            <button className="input-header-button">
+              <code>copy to clipboard</code>
+            </button>
+          </div>
+          <textarea type="text" className="input-field"></textarea>
+        </div>
+      </div>
+      <button disabled={!input} className="convert-button">
+        <code>convert</code>
+      </button>
+      <CSVReader onFileLoaded={(data, fileInfo) => setInput(data)} />
     </div>
   );
-}
+};
 
 export default App;
